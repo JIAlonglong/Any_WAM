7b7a98206c845ec56c09ac9dfca4dabac1c2bbca
JIAlonglong <2495477531@qq.com>
2026-07-10 15:15:50 +0800

    Allow RobotWin config to use pretrained env path

--- wan_va/configs/va_robotwin_cfg.py
+++ wan_va/configs/va_robotwin_cfg.py
@@ -6,7 +6,17 @@ from .shared_config import va_shared_cfg
 va_robotwin_cfg = EasyDict(__name__='Config: VA robotwin')
 va_robotwin_cfg.update(va_shared_cfg)
 
-va_robotwin_cfg.wan22_pretrained_model_name_or_path = "/path/to/pretrained/model"
+import os as _os
+va_robotwin_cfg.wan22_pretrained_model_name_or_path = _os.environ.get(
+    "WAN22_PRETRAINED_PATH",
+    _os.path.join(
+        _os.path.dirname(_os.path.abspath(__file__)),
+        "..",
+        "..",
+        "checkpoints",
+        "lingbot-va-posttrain-robotwin",
+    ),
+)
 
 va_robotwin_cfg.attn_window = 72
 va_robotwin_cfg.frame_chunk_size = 2
